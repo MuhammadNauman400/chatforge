c94b6fe6b3ab7f42841751da820c0d0124905f32
Muhammad Nauman <naumanmuhammad663@gmail.com>
2026-08-13 20:19:33 +0500

    Knowlegde base upload document- Design the page

--- resources/views/admin/backend/knowledge/knowledge_page.blade.php
+++ resources/views/admin/backend/knowledge/knowledge_page.blade.php
@@ -4,6 +4,18 @@
     <head>
         <meta name="csrf-token" content="{{ csrf_token() }}">
     </head>
+    <style>
+        .loading-spinner {
+            display: none;
+            text-align: center;
+            margin-top: 20px;
+        }
+
+        .loading-spinner.active {
+            display: block;
+        }
+    </style>
+
     <div class="page-container">
         <section id="knowledge-base" class="section-content">
             <h2 class="mb-2">Knowledge Base Management</h2>
@@ -25,6 +37,38 @@
 
                         <div id="uploadMessage" class="mt-3"></div>
                     </form>
+                </div>
+            </div>
+
+
+            <div class="card">
+                <div class="card-header">Uploaded Documents</div>
+                <div class="card-body">
+                    <div class="loading-spinner" id="documentsLoadingSpinner">
+                        <div class="spinner-border text-primary" role="status">
+                            <span class="visually-hidden">Loading....</span>
+                        </div>
+                        <p class="mt-2 text-muted">Loading Documents</p>
+                    </div>
+                    <div class="deleteMessage" class="mt-3"></div>
+                    <table class="table table-striped table-hover">
+                        <thead>
+                            <tr>
+                                <th>Document Name</th>
+                                <th>Status</th>
+                                <th>Uploaded At</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody id="documentsTableBody">
+                            <td>file name</td>
+                            <td><span class="badge bg-danger">processed</span></td>
+                            <td>13/8/26</td>
+                            <td>
+                                <button class="btn btn-sm btn-danger delete-document-btn">Delete</button>
+                            </td>
+                        </tbody>
+                    </table>
                 </div>
             </div>
 
