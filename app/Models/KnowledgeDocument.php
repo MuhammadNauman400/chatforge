a9beb8982897b528315198547a49aaee6f58dc0e
Muhammad Nauman <naumanmuhammad663@gmail.com>
2026-08-14 16:43:24 +0500

    Build relationships b/w user, company, chatbot, knowledge documents, knowledge chunks

--- app/Models/KnowledgeDocument.php
+++ app/Models/KnowledgeDocument.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class KnowledgeDocument extends Model
 {
     protected $guarded = [];
+
+    public function company(): BelongsTo {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function chatbots(): BelongsToMany {
+        return $this->belongsToMany(Chatbot::class, 'chatbot_knowledge_documents');
+    }
+
+    public function knowledgeChunks(): HasMany {
+        return $this->hasMany(KnowledgeChunk::class);
+    }
+
 }
