4e16aac9117e42508ae2bfe8b259754f3c6ac3a4
Muhammad Nauman <naumanmuhammad663@gmail.com>
2026-08-18 18:48:19 +0500

    User Profile Payment Plan - Done setup for upgrade blade page and show dynamic plan data

--- resources/views/client/backend/plans/upgrade.blade.php
+++ resources/views/client/backend/plans/upgrade.blade.php
@@ -7,57 +7,69 @@
 
 
                 <div class="row mt-sm-4 align-items-end justify-content-center my-3">
-                    <div class="col-lg-3">
-                        <div class="card h-100 overflow-hidden">
-                            <div class="card-header border-bottom border-dashed p-3 text-center">
-                                <h4 class="fw-bold fs-18">Free Plan</h4>
-                                <p class="mt-2 mb-0 text-muted">Tailored for
-                                    individual professionals and
-                                    hobbyists.</p>
-                            </div>
-                            <div class="card-body p-3">
-                                <div class="d-flex align-items-center gap-1">
-                                    <span class="text-muted fs-3 fw-semibold">$</span>
-                                    <h1 class="display-5 fw-semibold mb-0">0</h1>
-                                    <div class="d-block">
-                                        <p class=" fw-semibold mb-0">One-time payment</p>
-                                        <p class="text-muted mb-0">+plus local taxes</p>
-                                    </div>
-                                </div>
-                                <ul class="d-flex flex-column gap-2 mt-3 list-unstyled">
-                                    <li>
-                                        <i class="ti ti-point text-primary fs-4 align-middle me-1"></i>
-                                        Single user license
-                                    </li>
-                                    <li>
-                                        <i class="ti ti-point text-primary fs-4 align-middle me-1"></i>
-                                        Access to some components
-                                    </li>
-                                    <li>
-                                        <i class="ti ti-point text-primary fs-4 align-middle me-1"></i>
-                                        Lifetime access
-                                    </li>
-                                    <li>
-                                        <i class="ti ti-point text-primary fs-4 align-middle me-1"></i>
-                                        Single project
-                                    </li>
-                                    <li>
-                                        <i class="ti ti-point text-muted fs-4 align-middle me-1"></i>
-                                        No support
-                                    </li>
-                                    <li>
-                                        <i class="ti ti-point text-muted fs-4 align-middle me-1"></i>
-                                        No updates
-                                    </li>
 
-                                </ul>
-                            </div>
-                            <div class="card-footer">
-                                <a href="#!" class="btn btn-primary fw-semibold w-100">Buy
-                                    Now</a>
+                    @foreach ($plans as $plan)
+                        <div class="col-lg-3">
+                            <div class="card h-100 overflow-hidden">
+                                <div class="card-header border-bottom border-dashed p-3 text-center">
+                                    <h4 class="fw-bold fs-18">{{ $plan->name }} Plan</h4>
+                                    <p class="mt-2 mb-0 text-muted">
+                                        @if ($plan->name === 'Free')
+                                            Try it out and start business before you pay.
+                                        @elseif ($plan->name === 'Pro')
+                                            The foundational tools and support you need to build your business.
+                                        @else
+                                            Advanced tools and more support to help you scale.
+                                        @endif
+                                    </p>
+                                </div>
+                                <div class="card-body p-3">
+                                    <div class="d-flex align-items-center gap-1">
+                                        <span class="text-muted fs-3 fw-semibold">$</span>
+                                        <h1 class="display-5 fw-semibold mb-0">{{ $plan->price }}</h1>
+                                        {{-- <div class="d-block">
+                                            <p class=" fw-semibold mb-0">One-time payment</p>
+                                        </div> --}}
+                                    </div>
+                                    <ul class="d-flex flex-column gap-2 mt-3 list-unstyled">
+                                        <li>
+                                            <i class="ti ti-point text-primary fs-4 align-middle me-1"></i>
+                                            Knowledge Base: {{ $plan->knowledge_base }}
+                                        </li>
+                                        <li>
+                                            <i class="ti ti-point text-primary fs-4 align-middle me-1"></i>
+                                            Chatbots : {{ $plan->chat_bot }}
+                                        </li>
+                                        <li>
+                                            <i class="ti ti-point text-primary fs-4 align-middle me-1"></i>
+                                            AI-powered responses
+                                        </li>
+
+                                        <li>
+                                            <i class="ti ti-point text-primary fs-4 align-middle me-1"></i>
+                                            Document-based knowledge
+                                        </li>
+
+                                        <li>
+                                            <i class="ti ti-point text-primary fs-4 align-middle me-1"></i>
+                                            Website chatbot integration
+                                        </li>
+
+                                    </ul>
+                                </div>
+                                <div class="card-footer">
+                                    @if ($plan->name === Auth::user()->plan->name)
+                                        <button class="btn btn-secondary fw-semibold w-100" disabled>Current Plan</button>
+                                    @else
+                                        <a href="#!"
+                                            class="btn {{ $plan->name === 'Pro' ? 'btn-danger' : 'btn-primary' }} fw-semibold w-100">Buy
+                                            Now</a>
+                                    @endif
+
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    @endforeach
                 </div>
             </div> <!-- end col-->
         </div>
