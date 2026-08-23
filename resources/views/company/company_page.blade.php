ec86fab008ef9ca38d3a2704f9835276c137c916
Muhammad Nauman <naumanmuhammad663@gmail.com>
2026-08-23 17:30:55 +0500

    Add chatbot in company website. Load the css and js files for chatbot-widget

--- resources/views/company/company_page.blade.php
+++ resources/views/company/company_page.blade.php
@@ -14,6 +14,8 @@
     <!-- bootstrap  -->
     <link rel="stylesheet" href="{{asset('frontend/css/bootstrap.min.css')}}">
     <link rel="stylesheet" href="{{asset('frontend/css/style.min.css')}}">
+
+    <link href="{{asset('css/chatbot-widget.css')}}">
 </head>
 
 <body data-bs-spy="scroll" data-bs-target="#navbarCollapse">
@@ -680,6 +682,14 @@
             </div>
         </div>
     </footer>
+
+    @if ($company->chatbot_embed_code)
+
+    <div class="chatbot-embed-container">
+        {!! $company->chatbot_embed_code !!}
+    </div>
+        
+    @endif
     <script src="{{asset('frontend/js/bootstrap.bundle.min.js')}}"></script>
     <script src="https://cdn.lordicon.com/bhenfmcm.js"></script>
     <script src="{{asset('frontend/js/app.js')}}"></script>
